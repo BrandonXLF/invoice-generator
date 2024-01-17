7d85b278d5681bedff939b42c123e4821c9d36e2
Brandon Fowler <brandfowl@gmail.com>
2024-01-17 00:10:24 -0500

    Add footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script>
+	import Footer from './lib/Footer.svelte';
 	import Page from './lib/Page.svelte';
 </script>
 
 <Page />
+<Footer />
---
+++ src/lib/Footer.svelte
@@ -0,0 +1,13 @@
+<footer id="footer">
+	Quick and free invoice generator by Brandon Fowler.
+    <a href="https://github.com/brandonxlf/invoice-generator" target="_blank">View source code</a>.
+</footer>
+
+<style>
+	#footer {
+		width: 8.5in;
+        max-width: 100%;
+        margin: 1em auto 0;
+        color: #ddd;
+	}
+</style>
